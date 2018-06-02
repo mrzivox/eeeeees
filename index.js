363c0b8b22cc06e0852d0785b5a32c9cbd42976b
mrzivox <35635645+mrzivox@users.noreply.github.com>
2018-06-02 18:21:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ bot.on('message', message => {
   if(message.content.startsWith(prefix + "clear")) {
     if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGE")) return message.channel.send("Vous n'avez pas la permission !");
   
-    let args = message.content.split("").slice(1);
+    let args = message.content.split(" ").slice(1);
   
     if(!args[0]) return message.channel.send("Tu dois préciser un nombre de messages a supprimer !")
     message.channel.bulkDelete(args[0]).then(() => {
